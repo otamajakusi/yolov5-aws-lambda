52bd7640c5115b1591336a7344856d5451eb841d
hiroyuki.obinata <hiroyuki.obinata@gmail.com>
2022-10-22 21:11:15 +0900

    support yolov5 on aws lambda with opencv

--- hello_world/app.py
+++ hello_world/app.py
@@ -1,42 +1,162 @@
 import json
+import cv2
+import base64
+import time
+import numpy as np
 
 # import requests
 
 
+def build_model():
+    net = cv2.dnn.readNet("yolov5s.onnx")
+    net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
+    net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
+    return net
+
+
+INPUT_WIDTH = 640
+INPUT_HEIGHT = 640
+SCORE_THRESHOLD = 0.2
+NMS_THRESHOLD = 0.4
+CONFIDENCE_THRESHOLD = 0.4
+
+
+def detect(image, net):
+    blob = cv2.dnn.blobFromImage(
+        image, 1 / 255.0, (INPUT_WIDTH, INPUT_HEIGHT), swapRB=True, crop=False
+    )
+    net.setInput(blob)
+    preds = net.forward()
+    return preds
+
+
+def load_classes():
+    class_list = []
+    with open("classes.txt", "r") as f:
+        class_list = [cname.strip() for cname in f.readlines()]
+    return class_list
+
+
+def wrap_detection(input_image, output_data):
+    class_ids = []
+    confidences = []
+    boxes = []
+
+    rows = output_data.shape[0]
+
+    image_width, image_height, _ = input_image.shape
+
+    x_factor = image_width / INPUT_WIDTH
+    y_factor = image_height / INPUT_HEIGHT
+
+    for r in range(rows):
+        row = output_data[r]
+        confidence = row[4]
+        if confidence >= 0.4:
+
+            classes_scores = row[5:]
+            _, _, _, max_indx = cv2.minMaxLoc(classes_scores)
+            class_id = max_indx[1]
+            if classes_scores[class_id] > 0.25:
+
+                confidences.append(confidence)
+
+                class_ids.append(class_id)
+
+                x, y, w, h = row[0].item(), row[1].item(), row[2].item(), row[3].item()
+                left = int((x - 0.5 * w) * x_factor)
+                top = int((y - 0.5 * h) * y_factor)
+                width = int(w * x_factor)
+                height = int(h * y_factor)
+                box = np.array([left, top, width, height])
+                boxes.append(box)
+
+    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.25, 0.45)
+
+    result_class_ids = []
+    result_confidences = []
+    result_boxes = []
+
+    for i in indexes:
+        result_confidences.append(confidences[i])
+        result_class_ids.append(class_ids[i])
+        result_boxes.append(boxes[i])
+
+    return result_class_ids, result_confidences, result_boxes
+
+
+def format_yolov5(frame):
+    row, col, _ = frame.shape
+    _max = max(col, row)
+    result = np.zeros((_max, _max, 3), np.uint8)
+    result[0:row, 0:col] = frame
+    return result
+
+
+def yolov5(image):
+    colors = [(255, 255, 0), (0, 255, 0), (0, 255, 255), (255, 0, 0)]
+    class_list = load_classes()
+
+    net = build_model()
+
+    inputImage = format_yolov5(image)
+    outs = detect(inputImage, net)
+
+    class_ids, confidences, boxes = wrap_detection(inputImage, outs[0])
+
+    for (classid, confidence, box) in zip(class_ids, confidences, boxes):
+        color = colors[int(classid) % len(colors)]
+        cv2.rectangle(image, box, color, 2)
+        cv2.rectangle(
+            image, (box[0], box[1] - 20), (box[0] + box[2], box[1]), color, -1
+        )
+        cv2.putText(
+            image,
+            class_list[classid],
+            (box[0], box[1] - 10),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            0.5,
+            (0, 0, 0),
+        )
+    return image
+
+
+def base64_to_cv2(image_base64):
+    # base64 image to cv2
+    image_bytes = base64.b64decode(image_base64)
+    np_array = np.fromstring(image_bytes, np.uint8)
+    image_cv2 = cv2.imdecode(np_array, cv2.IMREAD_COLOR)
+    return image_cv2
+
+
+def cv2_to_base64(image_cv2):
+    # cv2 image to base64
+    image_bytes = cv2.imencode(".jpg", image_cv2)[1].tostring()
+    image_base64 = base64.b64encode(image_bytes).decode()
+    return image_base64
+
+
 def lambda_handler(event, context):
-    """Sample pure Lambda function
-
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
-
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
-
-    context: object, required
-        Lambda Context runtime methods and attributes
-
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
-
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
-
-    #     raise e
+    body = json.loads(event["body"])
+    image = body["image"]
+    image = yolov5(base64_to_cv2(image))
 
     return {
         "statusCode": 200,
-        "body": json.dumps({
-            "message": "hello world",
-            # "location": ip.text.replace("\n", "")
-        }),
+        "body": json.dumps(
+            {
+                "image": cv2_to_base64(image),
+            }
+        ),
     }
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input", help="input image", required=True)
+    parser.add_argument("--output", help="output image", required=True)
+    args = parser.parse_args()
+    image = yolov5(cv2.imread(args.input))
+    cv2.imwrite(args.output, image)
